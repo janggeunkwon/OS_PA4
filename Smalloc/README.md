0042e3fcfec98fadbe461e5a741f27d6e47769ca
janggeunkwon <rnjswkdrmsz@naver.com>
2019-06-17 22:44:50 +0900

    first version

---
+++ Smalloc/README.md
@@ -0,0 +1,5 @@
+2019-06-17
+OS_PA4
+21200055 janggeun kwon
+smalloc
+simple dynamic memory allocatiion library
---
+++ Smalloc/smalloc-1.1/test1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "smalloc.h"
+
+int 
+main()
+{
+	void *p1, *p2, *p3, *p4 ;
+
+	print_sm_containers() ;
+
+	p1 = smalloc(2000) ; 
+	printf("smalloc(2000)\n") ; 
+	print_sm_containers() ;
+
+	p2 = smalloc(2000) ; 
+	printf("smalloc(2000)\n") ; 
+	print_sm_containers() ;
+
+	sfree(p1) ; 
+	printf("sfree(%p)\n", p1) ; 
+	print_sm_containers() ;
+
+	p3 = smalloc(2000) ; 
+	printf("smalloc(2000)\n") ; 
+	print_sm_containers() ;
+
+	p4 = smalloc(2000) ; 
+	printf("smalloc(2000)\n") ; 
+	print_sm_containers() ;
+
+	p4 = smalloc(2000) ; 
+	printf("smalloc(2000)\n") ; 
+	print_sm_containers() ;
+	
+	//print_sm_uses();
+
+}
